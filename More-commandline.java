131cec14de7c7ca4eb716f8b30623d920f138a09
Shubh3819 <126344166+Shubh3819@users.noreply.github.com>
2025-02-09 03:00:52 +0530

    Create More-commandline.java

---
+++ More-commandline.java
@@ -0,0 +1,8 @@
+public class Demo4
+{
+public static void main(String[] args) {
+System.out.println(args[0]);
+System.out.println(args[1]);
+System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));
+}
+}
