ea883fd80fbb5e91834de4a469c0a331dcedb68d
Shubh3819 <126344166+Shubh3819@users.noreply.github.com>
2025-02-13 00:04:37 +0530

    Create Float-functions.java

---
+++ Float-functions.java
@@ -0,0 +1,16 @@
+class Demo{
+public static void main(String args[])
+{
+float a,b,sum,prod,diff,div;
+a=25.87f;
+b=45.00f;
+sum=a+b;
+prod=a*b;
+div=a/b;
+diff=a-b;
+System.out.println("The sum of 2 numbers is "+sum);
+System.out.println("The difference of 2 numbers is "+diff);
+System.out.println("The product of 2 numbers is "+prod);
+System.out.println("The division of 2 numbers is "+div);
+}
+}
