d075fe7107535476c1566cd491ff9cb5e2aeab1a
Shubh3819 <126344166+Shubh3819@users.noreply.github.com>
2025-02-12 23:59:08 +0530

    Create Interger-functions.java

---
+++ Interger-functions.java
@@ -0,0 +1,15 @@
+class arithm{
+public static void main(String args[])
+{
+int a,b,sum,prod,diff,div;
+a=25;b=45;
+sum=a+b;
+prod=a*b;
+div=a/b;
+diff=a-b;
+System.out.println("The sum of 2 numbers is "+sum);
+System.out.println("The difference of 2 numbers is "+diff);
+System.out.println("The product of 2 numbers is "+prod);
+System.out.println("The division of 2 numbers is "+div);
+}
+}
