b6f0ff72753cf94841ca806c17e826fa2e6cdb37
Shubh3819 <126344166+Shubh3819@users.noreply.github.com>
2025-02-15 00:48:57 +0530

    Create Display-val.java

---
+++ Display-val.java
@@ -0,0 +1,15 @@
+class dispval
+{
+public static void main(String args[])
+{
+int i;
+i=25;
+float j;
+j=22.4F;
+String k;
+k="a";
+System.out.println("The value of i is:"+i);
+System.out.println("The value of j is:"+j);
+System.out.println("The value of k is:"+k);
+}
+}
