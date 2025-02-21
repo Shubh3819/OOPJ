545951f2d39533e8eb74aaac1fea2ba62a53cd7b
Shubh3819 <126344166+Shubh3819@users.noreply.github.com>
2025-02-22 00:01:11 +0530

    Create Command-line-operations.java

---
+++ Command-line-operations.java
@@ -0,0 +1,10 @@
+public class Demo5
+{
+public static void main(String[] args) {
+float a, b, c;
+a = Float.parseFloat(args[0]);
+b = Float.parseFloat(args[1]);
+c =a + b;
+System.out.println("Addition of " + a + " and " + b + " = " + c);
+}
+}
